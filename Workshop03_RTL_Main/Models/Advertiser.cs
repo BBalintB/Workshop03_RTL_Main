e8332f9fab6fcc2a6c63551dfe04e78c07d5983e
BBalintB <bbalintb@gmail.com>
2022-10-05 10:23:45 +0200

    Added Advertiser

---
+++ Workshop03_RTL_Main/Models/Advertiser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Workshop03_RTL_Main.Models
+{
+    public class Advertiser:IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public int MinimumWage { get; set; }
+    }
+}
