db2bce98051425d3b2faee0adfec1f2cb87e4013
SalaminBence <bene.salamin@gmail.com>
2022-10-05 10:25:32 +0200

    Did Advert. class, Bardosi edittiondsf

---
+++ Workshop03_RTL_Main/Models/Advertisement.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Workshop03_RTL_Main.Models
+{
+    public class Advertisement
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [MaxLength(1000)]
+        public string Description { get; set; }
+        [Required]
+        public int Price { get; set; }
+    }
+}
